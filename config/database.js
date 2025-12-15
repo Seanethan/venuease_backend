52e0c02a45a656f52d15066e9dc19ffbc3b8a5ec
Shan <132747869+Seanethan@users.noreply.github.com>
2025-12-15 18:33:49 +0800

    Update database.js

--- config/database.js
+++ config/database.js
@@ -5,7 +5,7 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password: 'root', // Make sure this password is correct!
+    password: '1439', // Make sure this password is correct!
     database: 'VenuEase',
     waitForConnections: true,
     connectionLimit: 10,
